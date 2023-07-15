b3cc7d994940c6a35ec02bde9c7c1e6d0e35be12
MKYRENE <rodriguezm90@icloud.com>
2023-07-14 23:49:02 -0400

    changing applications name on readme and repo.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# dev_writers_hub
+# Dev_Blog_Post
 A CMS-style blog site for developers to publish tech articles, share insights, and engage with the coding community.
--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const express= require('express')
 //creating PORT # 
 const app= express();
 const PORT = process.env.PORT //Heroku will provide port number dynamically
+
+//Middleware
+
+//Routes
+
+// Startgin server 
+app.listen(PORT, () => console.log('Server started on port %s', PORT, 10));
