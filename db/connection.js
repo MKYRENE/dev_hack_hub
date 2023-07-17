e1fbe7dbd93f6b308b152c4f3433cac52713a5bc
MKYRENE <rodriguezm90@icloud.com>
2023-07-17 09:45:45 -0400

    forgot dialect

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,9 @@ const { Sequelize } = require('sequelize');
 
 
 const sequelize = process.env.JAWSDB_URL
-? new Sequelize(process.env.JAWSDB_URL)
+? new Sequelize(process.env.JAWSDB_URL, {
+  dialect: 'mysql'
+})
 : new Sequelize('dev_hack_hub', 'root', '', {
   host: '127.0.0.1',
   dialect: 'mysql'
