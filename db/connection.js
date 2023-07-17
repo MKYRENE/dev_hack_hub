dc68bacd6ef40628d4b39e5753daa8a0641c77e0
MKYRENE <rodriguezm90@icloud.com>
2023-07-17 09:44:55 -0400

    prepared sequelize connection for jawsdb

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('dev_hack_hub', 'root', '', {
+// require('dotenv').config();
+
+
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize('dev_hack_hub', 'root', '', {
   host: '127.0.0.1',
   dialect: 'mysql'
 });
